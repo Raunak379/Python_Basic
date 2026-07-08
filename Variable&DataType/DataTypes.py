e329639ed18171e277741acef8132dfe14ef4c0a
Raunak Kumar Singh <rs114408@gmail.com>
2026-07-08 20:06:07 +0530

    updated

---
+++ Variable&DataType/DataTypes.py
@@ -0,0 +1,6 @@
+#Create one variable of each basic data type (int, float, str).
+int = 10
+float = 10.5
+str = "hello"
+print(int,float,str)
+
---
+++ Variable&DataType/Variable.py
@@ -0,0 +1,24 @@
+#Create variables for your name, age, and city, then print them.
+Name = "Raunak"
+age = 21
+city = "siwan"
+print("name = ",Name, ",age = ",age,",city = ",city)
+
+
+#Assign three variables in a single line and print them.
+Name = "Raunak"
+Behavior = "is  a  good"
+Gender = "boy"
+print(Name,"",Behavior,"",Gender)
+
+#Assign the same value to three variables.
+Name = "Raunak"
+Naam = "Raunak"
+Name1 = "Raunak"
+print(Name,"",Naam,"",Name1)
+
+#Change the value of a variable and print it before and after the change.
+Num = 25
+print(Num)
+Num= 21
+print(Num)
