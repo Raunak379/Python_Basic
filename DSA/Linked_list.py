635b18bdbdbce864630f88081bfd6080fed9a9c3
Raunak Kumar Singh <rs114408@gmail.com>
2026-07-17 19:01:56 +0530

    Update Linked_list.py

--- DSA/Linked_list.py
+++ DSA/Linked_list.py
@@ -25,3 +25,80 @@ obj.insertAtEnd(10)
 obj.insertAtEnd(20)
 obj.insertAtEnd(30)
 obj.printerLL()
+
+#insertAtBeg
+class Node:
+    def __init__(self, info, next=None):
+        self.info = info
+        self.next = next
+class SinglyLinkedList:
+    def __init__(self, head=None):
+        self.head = head
+    def insertAtEnd(self, value):
+        temp = Node(value)
+        if self.head is None:
+            self.head = temp
+        else:
+            t1 = self.head
+            while t1.next is not None:
+                t1 = t1.next
+            t1.next = temp
+    def insertAtBeg(self,value):
+        temp = Node(value)
+        temp.next = self.head
+        self.head = temp
+
+    def printerLL(self):
+        t1 = self.head
+        while t1 is not None:
+            print(t1.info)
+            t1 = t1.next
+obj = SinglyLinkedList()
+obj.insertAtEnd(10)
+obj.insertAtEnd(20)
+obj.insertAtEnd(30)
+obj.insertAtBeg(5)
+obj.printerLL()
+
+#insertAtMid
+class Node:
+    def __init__(self, info, next=None):
+        self.info = info
+        self.next = next
+class SinglyLinkedList:
+    def __init__(self, head=None):
+        self.head = head
+    def insertAtEnd(self, value):
+        temp = Node(value)
+        if self.head is None:
+            self.head = temp
+        else:
+            t1 = self.head
+            while t1.next is not None:
+                t1 = t1.next
+            t1.next = temp
+    def insertAtBeg(self, value):
+        temp = Node(value)
+        temp.next = self.head
+        self.head = temp
+    def insertAtMid(self, value, x):
+        temp = Node(value)
+        t1 = self.head
+        while t1 is not None:
+            if t1.info == x:
+                temp.next = t1.next
+                t1.next = temp
+                break
+            t1 = t1.next
+    def printerLL(self):
+        t1 = self.head
+        while t1 is not None:
+            print(t1.info)
+            t1 = t1.next
+obj = SinglyLinkedList()
+obj.insertAtEnd(10)
+obj.insertAtEnd(20)
+obj.insertAtEnd(30)
+obj.insertAtBeg(5)
+obj.insertAtMid(40, 20)
+obj.printerLL()
